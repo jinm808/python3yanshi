6ed1806cc4cb5c3a1906288ce0e4b35bc74e1f7b
jim <hn_jinm@163.com>
2021-03-01 14:22:42 +0800

    lesson1

---
+++ main2.py
@@ -0,0 +1,31 @@
+# # f= open('test.txt','r+')
+# # f.write('world')
+# # print(f.fileno())
+#
+# a={1,12,12,3,3,4,5}
+# b=[1,2,2,3,3,4,5]
+# print(a)
+# print(b)
+print('hello,world!')
+
+# import requests
+# url='http://www.qq.com/'
+# strhtml=requests.get(url)
+# print(strhtml)
+
+import calendar
+# yy=int(input('输入年份：'))
+# mm=int(input('输入月份：'))
+# print(calendar.month(yy,mm))
+
+#print(calendar.month(2021,1))# yy=int(input('输入年份：'))
+# mm=int(input('输入月份：'))
+# print(calendar.month(yy,mm))
+
+#print(calendar.month(2021,1))
+
+#import math
+
+
+#Area=math.pi*2*2
+#print('mianjishi:'+Area)
